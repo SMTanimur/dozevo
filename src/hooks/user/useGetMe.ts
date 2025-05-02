a90f78ce288965d158065fd71ae87f0a59210f48
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2025-05-02 19:02:45 +0600

    Enhance useGetMe hook to include authentication token retrieval from cookies. Update query to be enabled only when the token is present, improving user session management.

--- src/hooks/user/useGetMe.ts
+++ src/hooks/user/useGetMe.ts
@@ -1,12 +1,14 @@
-
-import { userService } from "@/services";
-import { IUser } from "@/types";
-import { useQuery } from "@tanstack/react-query";
-
+import { userService } from '@/services';
+import Cookies from 'js-cookie';
+import { IUser } from '@/types';
+import { useQuery } from '@tanstack/react-query';
 
 export const useGetMe = () => {
+  const authToken = Cookies.get('Authentication');
+
   return useQuery<IUser, Error>({
     queryKey: [userService.getMyProfile.name],
     queryFn: () => userService.getMyProfile(),
+    enabled: !!authToken,
   });
 };
