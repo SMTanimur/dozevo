85175bd182448bdccadc10c609f2f3b663a25d9e
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2025-05-15 08:45:24 +0600

    Refactor next.config.ts to replace image domain configuration with remotePatterns for improved security and flexibility in image handling.

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,19 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      'res.cloudinary.com',
-      'lh3.googleusercontent.com',
-      'www.leafrootfruit.com.au',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.leafrootfruit.com.au',
+      },
     ],
   },
 };
