320e9f6b86cb6caff92a295180dc1af7a54c251b
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2025-07-01 20:12:21 +0600

    Add metadata for Login and Signup pages to enhance SEO

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,15 @@
 
 
+import { Metadata } from "next";
 import LoginScreen from "./screen/login-screen";
+import { constructMetadata } from "@/configs";
 
 
+export const metadata: Metadata = constructMetadata({
+  title: 'Login - Taskiya',
+  description: 'Login to your Taskiya account',
+  canonical: '/login',
+})
 
 export default function LoginPage() {
 
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,13 @@
-import SingupScreen from "./screen/singup-screen";
+import SingupScreen from './screen/singup-screen';
+import { Metadata } from 'next';
+import { constructMetadata } from '@/configs';
 
+export const metadata: Metadata = constructMetadata({
+  title: 'Signup - Taskiya',
+  description: 'Signup to your Taskiya account',
+  canonical: '/signup',
+});
 
 export default function SignupPage() {
-
-
-  return <SingupScreen/>
+  return <SingupScreen />;
 }
