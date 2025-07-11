4a58e39ebd732e5ab9ee37ca5877badc51183277
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2025-07-11 20:00:19 +0600

    Add metadata generation and async handling in SpacePage component
    - Introduced a new `generateMetadata` function to construct SEO metadata based on workspace and space IDs.
    - Updated the `SpacePage` component to handle async parameters and log workspace and space IDs.
    - Enhanced SEO capabilities by dynamically generating titles, descriptions, and canonical URLs for improved search visibility.

--- src/app/(worksapce)/[w_id]/s/[space_id]/page.tsx
+++ src/app/(worksapce)/[w_id]/s/[space_id]/page.tsx
@@ -1,6 +1,29 @@
+import { constructMetadata } from '@/configs';
 import SpaceScreen from './screen/space-screen';
 
-const SpacePage = () => {
+interface SpacePageProps {
+  params: {
+    w_id: string;
+    space_id: string;
+  };
+}
+
+export const generateMetadata = async ({ params }: SpacePageProps) => {
+  const { w_id, space_id } = await params;
+
+  return constructMetadata({
+    title: `Space ${space_id}`,
+    description: `Space ${space_id}`,
+    ogImage: `https://taskgen.io/api/v1/spaces/${space_id}/image`,
+    canonical: `https://taskgen.io/w/${w_id}/s/${space_id}`,
+  });
+};
+
+const SpacePage = async ({ params }: SpacePageProps) => {
+  const { w_id, space_id } = await params;
+
+  console.log(w_id, space_id);
+
   return <SpaceScreen />;
 };
 
